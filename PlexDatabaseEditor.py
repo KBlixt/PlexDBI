5c782174e33510eda236d3550775bb3d3f7ea31f
KBlixt <Sally3320>
2018-05-15 23:04:44 +0200

    major overhaul, it's now a class. although the script is in the class.

--- PlexDatabaseEditor.py
+++ PlexDatabaseEditor.py
@@ -34,7 +34,7 @@ class PlexDatabaseEditor:
         if not os.path.isfile('config'):
             f = open("config", "w+")
             if self.sudo:
-                os.system("sudo chmod 777 config")
+                os.system("sudo chown --=" + os.getcwd() + " config")
             f.write('\n[SETTINGS]')
             f.write('\nTMDB_API_KEY = ')
             f.write('\nMOVIE_LIBRARY_SECTION =')
